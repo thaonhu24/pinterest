1fd5845858ede3e354f3927a0f46d2d042fff542
thaonhu24 <thaonhu1240@gmail.com>
2022-02-25 15:39:03 +0700

    remove some file

--- src/components/hooks/useCusMemo.js
+++
@@ -1,9 +0,0 @@
-import {useState, useMemo} from "react";
-function useCusMemo(num){
-    const result = useMemo(
-        ()=>num+1
-        ,[num]
-    )
-    return result;
-}
-export default useCusMemo;
---
+++ src/store/containers/themeReducer.js
@@ -0,0 +1,4 @@
+let initState = {
+    color: "#FFFFFF"
+    };
+    
