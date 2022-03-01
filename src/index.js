0f99fb0ad05cec4b4987e142152affae4e096311
thaonhu24 <thaonhu1240@gmail.com>
2022-03-01 09:29:18 +0700

    Merge branch 'main' of https://github.com/thaonhu24/pinterest

--- src/index.js
+++ src/index.js
@@ -8,13 +8,8 @@ import {
   BrowserRouter
 } from 'react-router-dom';
 import './index.scss';
-<<<<<<< HEAD
-import Header from './components/Header'
-import ListImage from './components/ListImage';
-=======
 import Header from './components/NavHeader/Header/Header'
 import ListImage from './components/Image/ListImage/ListImage';
->>>>>>> b9b5ebd36607afdb197575e0f79d55b9a40813ce
 
 const handlerCloseOverlay = () => {
   document.getElementsByClassName('overlay')[0].classList.remove('over')
@@ -22,23 +17,15 @@ const handlerCloseOverlay = () => {
   document.getElementsByClassName('notify')[0].classList.remove('active')
 }
 ReactDOM.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-=======
   <BrowserRouter>
     <React.StrictMode>
->>>>>>> b9b5ebd36607afdb197575e0f79d55b9a40813ce
       <Header />
       <div className='overlay' onClick={handlerCloseOverlay}></div>
       <div className="main-content">
         <ListImage></ListImage>
       </div>
-<<<<<<< HEAD
-  </React.StrictMode>,    
-=======
     </React.StrictMode>
   </BrowserRouter>,
->>>>>>> b9b5ebd36607afdb197575e0f79d55b9a40813ce
   document.getElementById('root')
 );
 
